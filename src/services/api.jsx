d0dc5c7d2898b04f2b4a31cde3e86a9de4e0e0bf
Hugo Junior <hjunioreng@gmail.com>
2025-10-28 21:06:08 -0300

    deploy

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,38 +1,38 @@
-// // src/services/api.js
-// import axios from 'axios';
-
-// const api = axios.create({
-//   // baseURL: 'http://localhost:4000',
-//   baseURL: 'https://obra-sync-backend.onrender.com',
-
-// });
-
-// api.interceptors.request.use(config => {
-//   // Define Content-Type: application/json APENAS se não for FormData
-//   if (!(config.data instanceof FormData)) {
-//     config.headers['Content-Type'] = 'application/json';
-//   }
-//   // Se for FormData, deixa o axios definir automaticamente
-//   return config;
-// });
-
-// export default api;
-
-
 // src/services/api.js
-import axios from "axios";
+import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://obra-sync-backend.onrender.com",
+  // baseURL: 'http://localhost:4000',
+  baseURL: 'https://obra-sync-backend.onrender.com',
+
 });
 
-// 🔒 Intercepta todas as requisições e adiciona o token JWT
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+api.interceptors.request.use(config => {
+  // Define Content-Type: application/json APENAS se não for FormData
+  if (!(config.data instanceof FormData)) {
+    config.headers['Content-Type'] = 'application/json';
   }
+  // Se for FormData, deixa o axios definir automaticamente
   return config;
 });
 
 export default api;
+
+
+// // src/services/api.js
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "https://obra-sync-backend.onrender.com",
+// });
+
+// // 🔒 Intercepta todas as requisições e adiciona o token JWT
+// api.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// export default api;
