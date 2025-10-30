bb8de9930882e62e7ae3a09292757f1ccc495543
Hugo Junior <hjunioreng@gmail.com>
2025-10-29 21:09:51 -0300

    Corrige endpoint create-rdo e ajusta retorno de erro

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,22 +1,22 @@
 // src/services/api.js
-import axios from 'axios';
+// import axios from 'axios';
 
-const api = axios.create({
-  // baseURL: 'http://localhost:4000',
-  baseURL: 'https://obra-sync-backend.onrender.com',
+// const api = axios.create({
+//   // baseURL: 'http://localhost:4000',
+//   baseURL: 'https://obra-sync-backend.onrender.com',
 
-});
+// });
 
-api.interceptors.request.use(config => {
-  // Define Content-Type: application/json APENAS se não for FormData
-  if (!(config.data instanceof FormData)) {
-    config.headers['Content-Type'] = 'application/json';
-  }
-  // Se for FormData, deixa o axios definir automaticamente
-  return config;
-});
+// api.interceptors.request.use(config => {
+//   // Define Content-Type: application/json APENAS se não for FormData
+//   if (!(config.data instanceof FormData)) {
+//     config.headers['Content-Type'] = 'application/json';
+//   }
+//   // Se for FormData, deixa o axios definir automaticamente
+//   return config;
+// });
 
-export default api;
+// export default api;
 
 
 // // src/services/api.js
@@ -36,3 +36,22 @@ export default api;
 // });
 
 // export default api;
+
+
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://obra-sync-backend.onrender.com",
+
+
+});
+
+// 🔒 Intercepta todas as requisições e adiciona o token JWT
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
